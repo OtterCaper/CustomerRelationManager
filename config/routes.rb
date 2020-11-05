5abcbd30b0d8b3cd7a8302ad7e52b4649a11c5c4
OtterCaper <ian.de.groot.j@gmail.com>
2020-11-05 16:48:04 -0600

    Part 1 added partial for index and view of customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'customer/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :customer, only: %i[index show]
+
+  root to: 'customer#index'
 end
